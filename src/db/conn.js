2d82686449a00b6ef19b50423499228271487124
Bahnimaan_Kashyap <97360672+BkzWorld@users.noreply.github.com>
2024-05-27 23:10:22 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE_NAME}`, {
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
